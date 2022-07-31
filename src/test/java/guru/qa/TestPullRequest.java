6f740387640e9a5badad2a4fe223cb7209291215
renathi <griffmbt@gmail.com>
2022-08-01 01:25:48 +0300

    add void main

--- src/test/java/guru/qa/TestPullRequest.java
+++ src/test/java/guru/qa/TestPullRequest.java
@@ -1,2 +1,9 @@
-package guru.qa;public class TestPullRequest {
+package guru.qa;
+
+public class TestPullRequest {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i");
+        }
+    }
 }
